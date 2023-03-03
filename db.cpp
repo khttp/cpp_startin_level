f27ecacc5a38f6064729b9efa19ba74f46649fc1
abooh <abdelrahmankhattab9999@gmail.com>
2023-03-03 20:22:21 +0200

    addin some important files for c++

---
+++ db.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+#include <mysql/mysql.h>
+
+
+struct connection_details{
+    const char *server,*user,*password,*database;
+};
+
+
+MYSQL *mysql_connection_setup(struct connection_details mysql_details){
+    MYSQL*
+
+return 0;    
+}
+
+
+
+
+
