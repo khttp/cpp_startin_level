3aeb90522c320e994dcbb8ace91a3b8b183bdc9d
abooh <abdelrahmankhattab9999@gmail.com>
2023-03-18 06:07:24 +0200

    mysql connect then show all what u have databases

--- README.md
+++ README.md
@@ -5,4 +5,6 @@
 
  - https://zetcode.com/db/mysqlc/
  - https://lenngro.github.io/how-to/2021/01/05/Simple-TCPIP-Server-Cpp/
+ - https://www.youtube.com/watch?v=cNdlrbZSkyQ
+ - https://www.youtube.com/playlist?list=PLOxPIrpPpCcsO9fCjxLveALSvel5azMOB
  
--- mysql.cpp
+++ mysql.cpp
@@ -1,10 +1,11 @@
 #include <mysql/mysql.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <iostream>
 /// the way to compile these kind of files 
 
 //g++ mysql.cpp -o mysql -L/usr/include/mysql -lmysqlclient ;./mysql
+
 
 int main(int argc, char **argv)
 {
@@ -24,14 +25,39 @@ int main(int argc, char **argv)
       exit(1);
   }
 
-  if (mysql_query(con, "CREATE DATABASE testdb"))
+  if (mysql_query(con, "SHOW DATABASES"))
   {
       fprintf(stderr, "%s\n", mysql_error(con));
       mysql_close(con);
       exit(1);
   }
+  
 
+
+  MYSQL_RES *result = mysql_store_result(con);
+
+  if (result == NULL)
+  {
+      fprintf(stderr,"%s\n",mysql_error(con));
+      mysql_close(con);
+      exit(1);
+  }
+
+  int num_fields = mysql_num_fields(result);
+
+  MYSQL_ROW row;
+
+  while ((row = mysql_fetch_row(result)))
+  {
+      for(int i = 0; i < num_fields; i++)
+      {
+          std::cout << row[i] << std::endl;
+      }
+
+  }
+
+  mysql_free_result(result);
   mysql_close(con);
+
   exit(0);
 }
-
